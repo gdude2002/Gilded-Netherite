09bce8704f289b42328a9badaa96d55e3abb4ce1
aki~ <contact.sakirairo@gmail.com>
2020-10-07 16:26:01 -0300

    rebase

--- src/main/java/com/cumulusmc/gildednetherite/items/Gilded_Netherite_Item.java
+++ src/main/java/com/cumulusmc/gildednetherite/items/Gilded_Netherite_Item.java
@@ -3,7 +3,7 @@ package com.cumulusmc.gildednetherite.items;
 import net.minecraft.item.Item;
 
 public class Gilded_Netherite_Item extends Item {
-    public Gilded_Netherite_Item(Item.Settings settings) {
+    public Gilded_Netherite_Item(Settings settings) {
         super(settings);
     }
 }
