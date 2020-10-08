7cd7448419177fa078cedf47753051b801044e35
aki~ <contact.sakirairo@gmail.com>
2020-10-08 14:06:05 -0300

    mixin

--- src/main/java/com/cumulusmc/gildednetherite/GildedNetherite.java
+++ src/main/java/com/cumulusmc/gildednetherite/GildedNetherite.java
@@ -1,13 +1,27 @@
 package com.cumulusmc.gildednetherite;
 
 import com.cumulusmc.gildednetherite.items.RegisterItems;
+import com.google.common.collect.Lists;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+
 public class GildedNetherite implements ModInitializer {
+
+    public static final ArrayList<DamageSource> DAMAGE_SOURCES = Lists.newArrayList(
+            // All the damage sources we should be immune to
+
+            DamageSource.HOT_FLOOR,
+            DamageSource.IN_FIRE,
+            DamageSource.LAVA,
+            DamageSource.LIGHTNING_BOLT,
+            DamageSource.ON_FIRE
+    );
 
     public static final ItemGroup GILDED_NETHERITE_GROUP = FabricItemGroupBuilder.create(
             new Identifier("gildednetherite", "gilded_netherite_group"))
--- src/main/java/com/cumulusmc/gildednetherite/mixin/EntityMixin.java
+++ src/main/java/com/cumulusmc/gildednetherite/mixin/EntityMixin.java
@@ -1,19 +1,65 @@
 package com.cumulusmc.gildednetherite.mixin;
 
+import com.cumulusmc.gildednetherite.GildedNetherite;
+import com.cumulusmc.gildednetherite.items.RegisterItems;
+import com.google.common.collect.Lists;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.server.command.CommandOutput;
-import net.minecraft.util.Nameable;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Mixin(Entity.class)
-abstract class EntityMixin implements Nameable, CommandOutput {
-    @Inject(method = "isInvulnerableTo", at = @At(value = "TAIL", target = "net/minecraft/entity/Entity.isInvulnerableTo(Lnet/minecraft/entity/damage/DamageSource;)Z"))
-    private void injectInvulnerability(DamageSource inFire, CallbackInfoReturnable<Boolean> cir) {
+public abstract class EntityMixin {
 
+    @Shadow
+    public abstract Iterable<ItemStack> getArmorItems();
+
+    @Inject(cancellable = true, method = "isInvulnerableTo", at = @At(value = "HEAD", target = "net/minecraft/entity/Entity.isInvulnerableTo(Lnet/minecraft/entity/damage/DamageSource;)Z"))
+    private void injectInvulnerability(DamageSource damage, CallbackInfoReturnable<Boolean> cir) {
+        ArrayList<ItemStack> items = Lists.newArrayList(this.getArmorItems());
+
+        // Not simply booleans, for the sake of thread-safety (the compiler enforces this)
+        AtomicReference<Boolean> hasHelmet = new AtomicReference<>(false);
+        AtomicReference<Boolean> hasChestplate = new AtomicReference<>(false);
+        AtomicReference<Boolean> hasLeggings = new AtomicReference<>(false);
+        AtomicReference<Boolean> hasBoots = new AtomicReference<>(false);
+
+        items.forEach((item) -> {
+                    // Check each item and see if it's a piece of our armour. Note that we don't want to just check
+                    // if it's just "a piece of our armour" and increment a counter since we don't want people
+                    // just breaking the game to make this work
+
+                    if (item.getItem() == RegisterItems.GILDED_NETHERITE_HELMET) {
+                        hasHelmet.set(true);
+                    }
+
+                    if (item.getItem() == RegisterItems.GILDED_NETHERITE_CHESTPLATE) {
+                        hasChestplate.set(true);
+                    }
+
+                    if (item.getItem() == RegisterItems.GILDED_NETHERITE_LEGGINGS) {
+                        hasLeggings.set(true);
+                    }
+
+                    if (item.getItem() == RegisterItems.GILDED_NETHERITE_BOOTS) {
+                        hasBoots.set(true);
+                    }
+                }
+        );
+
+        if (hasHelmet.get() && hasChestplate.get() && hasLeggings.get() && hasBoots.get()) {
+            // If we have all the pieces of armour, check damage types.
+
+            if (GildedNetherite.DAMAGE_SOURCES.contains(damage)) {
+                cir.setReturnValue(true);
+            }
+        }
     }
-
 }
